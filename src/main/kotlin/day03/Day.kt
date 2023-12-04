1d0476fdd935fef264f1ca146a94f1d87043e4cf
Serge Khlebnikov <sekhleb@amazon.com>
2023-12-04 09:24:23 -0800

    Day 3 - Refactor

--- src/main/kotlin/day03/Day.kt
+++ src/main/kotlin/day03/Day.kt
@@ -3,10 +3,6 @@ package day03
 import java.util.Scanner
 import lines
 
-private val symbolRegex = Regex(""".*[^.\d]+.*""")
-private val gearRegex = Regex(""".*(\*).*""")
-private val numberRegex = Regex("""(\d+)""")
-
 private data class LocatedNumber(
     val row: Int,
     val range: IntRange,
@@ -16,8 +12,14 @@ private data class LocatedNumber(
 class Day(val input: Scanner) {
     private val grid = input.lines().toList()
     private val numbers = grid.indices.flatMap { index ->
-        gridSearch(numberRegex, index)
+        gridSearch(Regex("""(\d+)"""), index)
             .map { LocatedNumber(index, it.range, it.value.toInt()) }
+    }
+    private val symbolPoints = grid.indices.flatMap { index ->
+        gridSearch(Regex("""[^.\d]"""), index).map { it.range.first to index }
+    }
+    private val gearPoints = grid.indices.flatMap { index ->
+        gridSearch(Regex("""\*"""), index).map { it.range.first to index }
     }
 
     // ------------------------------------------------------------------------------------------------
@@ -25,22 +27,17 @@ class Day(val input: Scanner) {
     // ------------------------------------------------------------------------------------------------
 
     fun starOne() = numbers
-        .filterNot { num ->
-            num.row.yBounds
-                .map { gridSlice(it, num.range) }
-                .none { symbolRegex.matches(it) }
+        .filter {
+            symbolPoints.any { point -> point.isNear(it) }
         }
-        .sumOf { it.value }
+        .sumOf(LocatedNumber::value)
         .also(::println)
 
-    fun starTwo() = numbers
-        .fold(mutableMapOf<Pair<Int, Int>, MutableList<Int>>()) { gears, num ->
-            gears.apply {
-                for ((x, y) in num.findNearbyGears())
-                    getOrPut(x to y) { mutableListOf() }.add(num.value)
-            }
+    fun starTwo() = gearPoints
+        .map {
+            numbers.filter { num -> it.isNear(num) }.map { it.value }
         }
-        .values.filter { it.size == 2 }
+        .filter { it.size == 2 }
         .sumOf { it.reduce(Int::times) }
         .also(::println)
 
@@ -48,18 +45,14 @@ class Day(val input: Scanner) {
     // Helpers
     // ------------------------------------------------------------------------------------------------
 
-    private fun LocatedNumber.findNearbyGears() =
-        row.yBounds.flatMap { y ->
-            gridSearch(gearRegex, y, range)
-                .map { range.xBounds.first + it.range.first to y }
-        } // list of (x, y) of gears nearby
-
     private val Int.yBounds // select rows
         get() = setOf(this, minOf(this + 1, grid.lastIndex), maxOf(this - 1, 0))
     private val IntRange.xBounds // select indices
         get() = maxOf(first - 1, 0)..minOf(last + 1, grid.lastIndex)
 
-    private fun gridSlice(row: Int, range: IntRange) = grid[row].substring(range.xBounds)
+    private fun Pair<Int, Int>.isNear(number: LocatedNumber) =
+        first in number.range.xBounds && second in number.row.yBounds
+
     private fun gridSearch(regex: Regex, row: Int, range: IntRange = 0..grid[row].lastIndex) =
-        regex.findAll(gridSlice(row, range)).mapNotNull { it.groups.last() }
+        regex.findAll(grid[row].substring(range.xBounds)).mapNotNull { it.groups.last() }
 }
